039cedf0af24ceeb2b21c1ac579b34f53ccc1fbe
Plamena Koycheva <prodanowa@gmail.com>
2025-05-27 13:58:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello C#");
+double distance = double.Parse(Console.ReadLine());
+double time = double.Parse(Console.ReadLine());
+double speed = distance / time;
+Console.WriteLine($"{speed:F2}");
