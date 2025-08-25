b11e1c070ef97438ad8c6cb5b663524eef10a9fd
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:42:02 +0300

    Add files via upload

---
+++ 01.ChangeList/Program.cs
@@ -0,0 +1,38 @@
+﻿//Write a program that:
+//•	Reads a list of integers from the console and receives commands to manipulate the list
+//•	Then until you receive "end", you will receive different commands:
+//o   "Delete {element}" – delete all elements in the list, which are equal to the given element
+//o	"Insert {element} {position}" – insert the element at the given position
+//•	When you receive the "end" command, print the final state of the list (separated by spaces)
+
+
+
+List<int> numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+string command =  Console.ReadLine();
+while (command != "end")
+{
+    string[] commandParts = command.Split();
+    string commandName = commandParts[0];
+    if (commandName == "Delete")
+    {
+        int numberToBeRemoved = int.Parse(commandParts[1]);
+        numbers.RemoveAll(number => number == numberToBeRemoved);
+
+
+    }
+    else if (commandName == "Insert")
+    {
+        int numberToInsert = int.Parse(commandParts[1]);
+        int positionToInsert = int.Parse(commandParts[2]);
+        numbers.Insert(positionToInsert, numberToInsert);
+
+    }
+
+
+        command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
---
+++ 02.ListOfProducts/Program.cs
@@ -0,0 +1,21 @@
+﻿//Write a program that:
+//•	Read an integer number n and n lines of products
+//•	Print a numbered list of all the products ordered by name
+
+int countProducts = int.Parse(Console.ReadLine());
+List<string> products = new List<string>();
+
+for (int product = 1; product <= countProducts; product++)
+{
+    string productName = Console.ReadLine();
+    products.Add(productName);
+}
+products.Sort();
+
+int number = 1;
+
+foreach (string product in products)
+{
+    Console.WriteLine(number + "." + product);
+    number++;
+}
