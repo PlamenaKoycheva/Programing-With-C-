8ad1e9b378953fd27cf348208ae460fd0bda95a0
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:40:28 +0300

    Add files via upload

---
+++ 05.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,48 @@
+﻿//Write a program that multiplies the sum of all even digits of a number by the sum of all odd digits of the same number:
+//•	Read an integer number from the console
+//•	Create a method called GetMultipleOfEvenAndOdds()
+//•	Create a method GetSumOfEvenDigits()
+//•	Create GetSumOfOddDigits()
+//•	You may need to use Math.Abs() for negative numbers
+
+
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+
+Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+static int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 == 0)
+        {
+            sum += lastDigit;
+        }
+        number /= 10;
+    }
+    return sum;
+}
+static int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 != 0)
+        {
+            sum += lastDigit;
+        }
+        number /= 10;
+    }
+    return sum;
+
+}
+static  int GetMultipleOfEvenAndOdds(int number)
+{
+    return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+
+}
+
---
+++ 06.Orders/Program.cs
@@ -0,0 +1,53 @@
+﻿//Write a program that:
+//•	Reads a string on the first line from the console, representing a product:
+//"coffee",  "water", "coke" or "snacks"
+//•	Reads an integer on the second line from the console, representing the quantity of the product
+//•	Create a method that calculates and prints the total price of an order
+//•	The method should receive two parameters: product and quantity
+//•	The prices for a single item of each product are:
+//	coffee – 1.50
+//	water – 1.00
+//	coke – 1.40
+//	snacks – 2.00
+//•	Print the result, formatted to the second digit
+
+
+
+string product = Console.ReadLine();
+
+int quantity = int.Parse(Console.ReadLine());
+
+TotalPrice(product, quantity);
+
+static void TotalPrice(string? product, int quantity)
+{
+    
+    double totalPrice = 0;
+
+    if (product == "coffee")
+    {
+        double price = quantity * 1.50;
+        totalPrice += price;
+       
+    }
+    else if (product == "water")
+    {
+        double price = quantity * 1.00;
+        totalPrice += price;
+
+    }
+    else if (product == "coke")
+    {
+        double price = quantity * 1.40;
+        totalPrice += price;
+
+    }
+    else if (product == "snacks")
+    {
+        double price = quantity * 2.00;
+        totalPrice += price;
+
+    }
+    Console.WriteLine($"{totalPrice:F2}");
+
+}
