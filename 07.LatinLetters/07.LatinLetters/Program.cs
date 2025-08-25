674c3cccec2892630647ff8a2c633678103ba8d4
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:22:36 +0300

    Add files via upload

---
+++ 07.LatinLetters/07.LatinLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿//Write a program to print the Latin letters in certain range:
+
+//· Read two letters (chars), each on separate line
+
+//· Print all letters in the specified range inclusively
+
+char letter1 = char.Parse(Console.ReadLine());
+char letter2 = char.Parse(Console.ReadLine());
+for (char i = letter1; i <= letter2; i++)
+{
+    Console.Write(i + " ");
+}
