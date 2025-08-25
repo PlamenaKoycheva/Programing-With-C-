29c12830f391a3a43f0a7be8476aed49eb5c94d9
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:26:41 +0300

    Add files via upload

---
+++ 04.VowelSum/04.VowelSum/Program.cs
@@ -0,0 +1,29 @@
+﻿int countChar = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < countChar; i++)
+{
+    char symbol = char.Parse(Console.ReadLine());
+    if (symbol == 'a')
+    {
+        sum += 1;
+    }
+    else if (symbol == 'e')
+    {
+        sum += 2;
+    }
+    else if (symbol == 'i')
+    {
+        sum += 3;
+    }
+    else if (symbol == 'o')
+    {
+        sum += 4;
+    }
+    else if (symbol == 'u')
+    {
+        sum += 5;
+    }
+
+
+}
+Console.WriteLine(sum);
