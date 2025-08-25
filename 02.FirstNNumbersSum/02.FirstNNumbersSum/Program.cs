b650bb89c5f474184e6dc78ccc0e5384508fa96b
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:25:05 +0300

    Add files via upload

---
+++ 02.FirstNNumbersSum/02.FirstNNumbersSum/Program.cs
@@ -0,0 +1,28 @@
+﻿//Write a program, which sums the numbers 1…n:
+
+//· Reads an integer number n from the console
+
+//· Sums all numbers from 1 to n
+
+//· Prints the numbers and the sum on the console as shown below
+
+using System.Diagnostics.CodeAnalysis;
+
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+  
+    Console.Write(i);
+    sum += i;
+    if (i < n)
+    {
+        Console.Write("+");
+    }
+  
+   
+    
+}
+Console.Write($"={sum}");
+
