6555949c39f0b77dd5bf99e00691e0ddb71f7865
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:12:00 +0300

    Add files via upload

---
+++ 03.OddNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+while (num % 2 == 0)
+{
+     num = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(num);
---
+++ 04.Sequence2KPlus1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Write a program to print a sequence of numbers:
+//•	The first number is 1
+//•	Each next number is 2 times the previous number + 1
+//•	Read an integer number n from the console – the max number
+//•	Print the elements of the sequence (starting with 1), which are ≤ n
+
+int range = int.Parse(Console.ReadLine());
+int number = 1;
+while (number <= range)
+{
+    Console.WriteLine(number);
+    number = number * 2 + 1;
+}
