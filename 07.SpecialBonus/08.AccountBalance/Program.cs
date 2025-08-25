0173772abea2c36e13ace3b5ff9ee1c78abdcccd
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:35:08 +0300

    Add files via upload

---
+++ 07.SpecialBonus/08.AccountBalance/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Write a program to calculate an account balance:
+//•	Read a sequence of incomes / expenses, until "End" is read
+//•	Add the money to the balance (starting form 0)
+//•	Print "Increase: {money}" or "Decrease: {money}", where value is formatted to the second decimal digit
+//•	Finally, print the account balance, formatted to the second decimal digit in the following format: "Balance: {account balance}"
+
+string command = Console.ReadLine();
+double balance = 0;
+while (command != "End")
+{
+    double money = double.Parse(command);
+    
+    if (money > 0)
+    {
+        balance += money;
+        Console.WriteLine($"Increase: {money:F2}");
+    }
+    else if (money < 0)
+    {
+        balance += money;
+        Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+    }
+        command = Console.ReadLine();
+}
+Console.WriteLine($"Balance: {balance:F2}");
