ac36dc1d660bf0cd41831f0e36d2af78f56d0a20
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:36:54 +0300

    Add files via upload

---
+++ 03.PrintingTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿//Write a program that:
+//•	Reads an integer number N from the console
+//•	Create a method for printing triangle depending on value of the number N
+
+int n = int.Parse(Console.ReadLine());
+
+PrintingTriangle1(n);
+PrintingTriangle2(n);
+
+
+static void PrintingTriangle1(int number)
+{
+    for (int i = 1; i <= number; i++ )
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write($"{j} ");
+        }
+        Console.WriteLine();
+    }
+}
+static void PrintingTriangle2(int number)
+{
+    for (int i = number - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write($"{j} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 04.CalculateRectangleArea/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Write a program that:
+//•	Reads two integer numbers from the console: width and length
+//•	Create a method which returns rectangle area with given width and length
+//Hint: Rectangle area can be calculated when you multiply width and length of the rectangle. 
+int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+
+int area =PrintRectangleArea(width, length);
+Console.WriteLine(area);
+
+static int PrintRectangleArea(int width, int length)
+{
+    return width * length;
+}
