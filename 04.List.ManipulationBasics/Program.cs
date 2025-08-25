28f4175bd14798f77ebd44fc6e6aa024d7079d93
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:43:53 +0300

    Add files via upload

---
+++ 04.List.ManipulationBasics/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+//Write a program that: 
+//•	Reads a list of integers
+//•	Then until you receive "end", you will receive different commands:
+//o   "Add {number}": add a number to the end of the list
+//o	"Remove {number}": remove a number from the list
+//o	"RemoveAt {index}": remove a number at a given index
+//o	"Insert {number} {index}": insert a number at a given index
+//•	When you receive the "end" command, print the final state of the list (separated by spaces)
+//Note: All the indices will be valid!
+
+
+List<int> numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+string command  = Console.ReadLine();
+
+while (command != "end")
+{
+    string[] commandArg = command.Split(" ");
+    string input = commandArg[0];
+    if (input == "Add")
+    {
+        int number = int.Parse(commandArg[1]);
+        numbers.Add(number);
+    }
+    else if (input == "Remove")
+    {
+        int number = int.Parse(commandArg[1]);
+        numbers.Remove(number);
+    }
+    else if (input == "RemoveAt")
+    {
+        int index = int.Parse(commandArg[1]);
+        numbers.RemoveAt(index);
+    }
+    else if (input == "Insert")
+    {
+        int number = int.Parse(commandArg[1]);
+        int index = int.Parse(commandArg[2]);
+        numbers.Insert(index, number);
+    }
+
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
---
+++ 05.BombNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿//Write a program that:
+//•	Reads a sequence of integer numbers from the first line of the console
+//•	Read a special bomb number (integer) and its power (integer) from the second line of the console
+//•	Detonate every occurrence of the special bomb number and according to its power - his neighbors from left and right
+//•	 Detonations are performed from left to right, and all detonated numbers disappear
+//•	Print the sum of the remaining elements in the sequence
+
+
+List<int> numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+int[] bombAndPower = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+int bombNumber = bombAndPower[0];
+int bombPower = bombAndPower[1];
+
+while (numbers.Contains(bombNumber))
+{
+    int bombIndex = numbers.IndexOf(bombNumber);
+
+    int startIndex = bombIndex - bombPower;
+    int count = bombPower * 2 + 1;
+
+    if (startIndex < 0)
+    {
+
+        count += startIndex;
+        startIndex = 0;
+    }
+        
+    if (bombIndex + bombPower > numbers.Count)
+    {
+        int indexToRemove = bombIndex + bombPower - numbers.Count + 1;
+        count -= indexToRemove;
+    }
+    
+
+    numbers.RemoveRange(startIndex, count);
+
+
+}
+
+Console.WriteLine(numbers.Sum());
