c02e29fe0afe3ee46c3d014d8f6c50f6d7150a2a
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:09:59 +0300

    Add files via upload

---
+++ 02. DecreasingNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿//Write a program that:
+//•	Reads an integer number N
+//•	Print the numbers from N down to 1 (inclusively), each on separate line
+//Note: N will always be bigger than 1.
+int num = int.Parse(Console.ReadLine());
+while (num >= 1)
+{
+    Console.WriteLine(num);
+    num--;
+}
