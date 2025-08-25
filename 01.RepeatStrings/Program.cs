173ea35b05614efafba26a2c897921f27006b9d2
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:52:09 +0300

    Add files via upload

---
+++ 01.RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that:
+//•	Reads a series of strings from the console, until you receive an "end" command
+//•	Reverse given strings
+//•	Print each pair (old text and reversed text) on a separate line in the format:
+// "{word} = {reversed word}"
+
+using System.Globalization;
+
+string command = Console.ReadLine();
+
+while(command != "end")
+{
+    string result = "";
+
+    for (int i = command.Length - 1;  i >= 0; i--)
+    {
+        result += command[i];
+        
+    }
+    Console.WriteLine($"{command} = {result}");
+    
+
+    command = Console.ReadLine();
+}
---
+++ 10.CompanyUsers/Program.cs
@@ -0,0 +1,41 @@
+﻿//Create a program that keeps information about companies and their employees. 
+//You will be receiving a company name and an employee's id, until you receive the "End" command. Add each employee to the given company.
+//Keep in mind that a company cannot have two employees with the same id.
+//When you finish reading the data, print the company's name and each employee's id in the following format:
+//{ companyName}
+//-- { id1}
+//-- { id2}
+//-- { idN}
+//Input / Constraints
+//•	Until you receive the "End" command, you will be receiving input in the format: "{companyName} -> {employeeId}".
+//•	The input always will be valid.
+
+Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] parts = input.Split(" -> ");
+    string company = parts[0];
+    string employeeId = parts[1];
+
+    if (!companies.ContainsKey(company))
+    {
+        companies[company] = new List<string>();
+    }
+
+    if (!companies[company].Contains(employeeId))
+    {
+        companies[company].Add(employeeId);
+    }
+    input = Console.ReadLine();
+}
+
+foreach (var company in companies)
+{
+    Console.WriteLine(company.Key);
+    foreach (var employeeId in company.Value)
+    {
+        Console.WriteLine($"-- {employeeId}");
+    }
+}
