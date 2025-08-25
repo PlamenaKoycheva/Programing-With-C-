e696d3af7214121614373b5e6bdd34c5d95f6e00
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 14:30:54 +0300

    Add files via upload

---
+++ 05.NumberPyramid/Program.cs
@@ -0,0 +1,25 @@
+﻿//Write a program that:
+//•	Reads an integer number n from the console 
+//•	Prints a pyramid of numbers as shown in the examples
+
+
+int num = int.Parse(Console.ReadLine());
+int counter = 0;
+for (int row = 1; row <= num; row++)
+{
+    for (int col = 1; col <= row;col++)
+    {
+        counter++;
+        Console.Write($"{counter} ");
+        if (counter == num)
+        {
+            break;
+        }
+    }
+    if (counter == num)
+    {
+        break;
+    }
+    Console.WriteLine();
+
+}
